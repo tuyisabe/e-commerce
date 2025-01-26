33cb04d0a954d704865fba943c0850842f1a6c13
Your Name <user@example.com>
2025-01-26 04:10:19 +0200

    refactor: move Firebase config to environment variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,14 +4,14 @@ import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdCSzzo9QaCxBmrjtsw69Sxo2UJINGuCk",
-  authDomain: "sureshop-7a946.firebaseapp.com",
-  databaseURL: "https://sureshop-7a946-default-rtdb.firebaseio.com",
-  projectId: "sureshop-7a946",
-  storageBucket: "sureshop-7a946.firebasestorage.app",
-  messagingSenderId: "170105734610",
-  appId: "1:170105734610:web:d387ab31441053bfe2740b",
-  measurementId: "G-QXHWJ2K4VK"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
